b51a351e233dbdba6187299e073377b260350a13
nihatran <89045243+nihatran@users.noreply.github.com>
2025-07-24 14:50:27 -0400

    Set up project

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { Card } from "./Card";
+import "./App.css";
+
+function App() {
+  return <Card></Card>;
+}
+
+export default App;
